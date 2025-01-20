95167c4c1f0d8635329727ef2c94d48b961d5843
Presab Khadka <presabkhadka30@gmail.com>
2025-01-21 02:26:08 +0545

    feat - add treasure

--- server/controller/promoterController.ts
+++ server/controller/promoterController.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import { Promoter } from "../db/db";
+import { Promoter, Treasure } from "../db/db";
 
 dotenv.config();
 
@@ -75,56 +75,104 @@ export async function promoterLogin(
 ): Promise<void> {
   try {
     const userEmail = req.body.email;
-  const userPassword = req.body.password;
+    const userPassword = req.body.password;
 
-  const existingUser = await Promoter.findOne({
-    userEmail,
-  });
-
-  if (userEmail == "" || userPassword == "") {
-    res.json(403).json({
-      msg: "input fields cannot be left empty",
+    const existingUser = await Promoter.findOne({
+      userEmail,
     });
-    return;
-  }
 
-  if (!existingUser) {
-    res.status(401).json({
-      msg: "promoter doesnt exist with such email",
-    });
-    return;
-  } else {
-    if (typeof existingUser.userPassword !== "string") {
-      res.status(401).json({
-        msg: "password invalid or missing",
+    if (userEmail == "" || userPassword == "") {
+      res.json(403).json({
+        msg: "input fields cannot be left empty",
       });
       return;
     }
 
-    let passwordMatch = await bcrypt.compare(
-      userPassword,
-      existingUser.userPassword
-    );
-
-    if (!passwordMatch) {
+    if (!existingUser) {
       res.status(401).json({
-        msg: "password doesn't matches",
+        msg: "promoter doesnt exist with such email",
       });
       return;
+    } else {
+      if (typeof existingUser.userPassword !== "string") {
+        res.status(401).json({
+          msg: "password invalid or missing",
+        });
+        return;
+      }
+
+      let passwordMatch = await bcrypt.compare(
+        userPassword,
+        existingUser.userPassword
+      );
+
+      if (!passwordMatch) {
+        res.status(401).json({
+          msg: "password doesn't matches",
+        });
+        return;
+      }
+
+      let payload: payload = { userEmail };
+      let token = jwt.sign(payload, process.env.JWT_SECRET || "defaultkey");
+
+      res.status(200).json({
+        token,
+      });
     }
-
-    let payload: payload = { userEmail };
-    let token = jwt.sign(payload, process.env.JWT_SECRET || "defaultkey");
-
-    res.status(200).json({
-      token,
-    });
-  }
   } catch (error) {
-   console.log(error);
+    console.log(error);
     res.status(500).json({
-        msg: "something wrong with the server at the moment"
-    })
+      msg: "something wrong with the server at the moment",
+    });
   }
 }
 
+export async function addTreasure(req: Request, res: Response) {
+  try {
+    const treasureName = req.body.treasureName;
+    const treasureLocation = req.body.treasureLocation;
+    const treasureDescription = req.body.treasureDescription;
+    const treasureType = req.body.treasureType;
+    const treasureImage = req.file?.path;
+
+    const existingTreasure = await Treasure.findOne({
+      treasureName,
+    });
+
+    if (
+      treasureName == "" ||
+      treasureLocation == "" ||
+      treasureDescription == "" ||
+      treasureType == "" ||
+      treasureImage == ""
+    ) {
+      res.status(400).json({
+        msg: "input fields cannot be left empty",
+      });
+    }
+
+    if (existingTreasure) {
+      res.status(409).json({
+        msg: "treasure already exists",
+      });
+    } else {
+      const newTreasure = await Treasure.create({
+        treasureName,
+        treasureLocation,
+        treasureDescription,
+        treasureType,
+        treasureImage,
+      });
+      res.status(200).json({
+        msg: "treasure created successfully",
+        newTreasure,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "something wrong with the server at the moment",
+    });
+  }
+}
