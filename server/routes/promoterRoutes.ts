e73d4bde36b5607a3428890fc9601764f921b6d6
Presab Khadka <presabkhadka30@gmail.com>
2025-01-21 02:26:14 +0545

    installed multer

--- server/routes/promoterRoutes.ts
+++ server/routes/promoterRoutes.ts
@@ -1,8 +1,23 @@
 import { Router, type Request, type Response } from "express";
-import { promoterSignup, promoterLogin } from "../controller/promoterController";
+import multer from "multer";
+import {
+  promoterSignup,
+  promoterLogin,
+  addTreasure,
+} from "../controller/promoterController";
+import promoterMiddleware from "../middleware/promoterMiddleware";
+
+const upload = multer({ dest: "upload/" });
+
 const promoterRouter = Router();
 
 promoterRouter.post("/signup", promoterSignup);
-promoterRouter.post("/login", promoterLogin)
+promoterRouter.post("/login", promoterLogin);
+promoterRouter.post(
+  "/add-treasure",
+  promoterMiddleware,
+  upload.single("treasureImage"),
+  addTreasure
+);
 
 export { promoterRouter };
