3ae6301c6d55cf1b6746ee670a1aa331753584b1
Presab Khadka <presabkhadka30@gmail.com>
2025-01-21 02:25:25 +0545

    updt - middleware change

--- server/middleware/explorerMiddleware.ts
+++ server/middleware/explorerMiddleware.ts
@@ -1,18 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { Explorer } from "../db/db";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 async function explorerMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const username = req.headers.username;
-  const password = req.headers.password;
-  const existingUser = await Explorer.findOne({
-    userName: username,
-    userPassword: password,
-  });
+  const token = req.headers.authorization;
+  const word = token?.split(" ");
 
+  if (word?.length !== 2 || word[0] !== "Bearer") {
+    return res.status(401).json({
+      msg: "invalid token format",
+    });
+  }
+  const cleanToken = word[1];
+  const jwtPass = process.env.JWT_SECRET || "defaultkey";
+  const decoded = jwt.verify(cleanToken, jwtPass);
+  const existingUser = await Explorer.findOne({});
   if (!existingUser) {
     return res.status(401).json({
       msg: "explorer not found in db",
