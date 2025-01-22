7fa8802922be4fd41b029a16cbdd0e92bab1c275
Presab Khadka <presabkhadka30@gmail.com>
2025-01-23 00:26:15 +0545

    added routes to search treasure fn

--- server/routes/explorerRoutes.ts
+++ server/routes/explorerRoutes.ts
@@ -1,8 +1,14 @@
 import { Router, type Request, type Response } from "express";
-import { explorerSignup, explorerLogin } from "../controller/explorerController";
+import {
+  explorerSignup,
+  explorerLogin,
+  searchTreasure,
+} from "../controller/explorerController";
+import explorerMiddleware from "../middleware/explorerMiddleware";
 const explorerRouter = Router();
 
 explorerRouter.post("/signup", explorerSignup);
 explorerRouter.post("/login", explorerLogin);
+explorerRouter.get("/search-treasures", explorerMiddleware, searchTreasure);
 
 export { explorerRouter };
