9a69481c866e1a0587c5961a8a0bcdcaa0c14b03
Presab Khadka <presabkhadka30@gmail.com>
2025-02-03 21:12:03 +0545

    feat - craeting promoter dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/register";
 import PromoterLogin from "./pages/promoterLogin";
 import PromoterRegister from "./pages/promoterRegister";
 import PromoterVerify from "./pages/promoterVerify";
+import PromoterDashboard from "./pages/promoterDashboard";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         { path: "/explorer/signup", element: <Register /> },
         { path: "/promoter/signup", element: <PromoterRegister /> },
         { path: "/promoter/verify", element: <PromoterVerify /> },
+        { path: "/promoter/dashboard", element: <PromoterDashboard /> },
       ])}
     ></RouterProvider>
   );
---
+++ client/src/pages/promoterDashboard.tsx
@@ -0,0 +1,9 @@
+function PromoterDashboard() {
+  return (
+    <div>
+      <h1>this will be the promoter dashboard page</h1>
+    </div>
+  );
+}
+
+export default PromoterDashboard;
