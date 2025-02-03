b757dfac539afb6ca845f60b05eda9d977361bc7
Presab Khadka <presabkhadka30@gmail.com>
2025-02-03 12:13:44 +0545

    updt - updated routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,19 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Otp from "./pages/otp";
 import PromoterLogin from "./pages/promoterLogin";
+import PromoterRegister from "./pages/promoterRegister";
 
 function App() {
   return (
-    <RouterProvider router={createBrowserRouter([
-      {path: '/user/login', element:<Login/>},
-      {path: '/promoter/login', element:<PromoterLogin/>},
-      {path: '/signup', element:<Register/>},
-      {path: '/verify', element: <Otp/>}
-    ])}>
-
-    </RouterProvider>
+    <RouterProvider
+      router={createBrowserRouter([
+        { path: "/explorer/login", element: <Login /> },
+        { path: "/promoter/login", element: <PromoterLogin /> },
+        { path: "/explorer/signup", element: <Register /> },
+        { path: "/promoter/signup", element: <PromoterRegister /> },
+        { path: "/promoter/verify", element: <Otp /> },
+      ])}
+    ></RouterProvider>
   );
 }
 
