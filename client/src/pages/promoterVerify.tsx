17ecc0154ed40ff18d1acdd096fe788057927b5b
Presab Khadka <presabkhadka30@gmail.com>
2025-02-03 18:21:44 +0545

    feat - promoter otp verification

---
+++ client/src/pages/promoterVerify.tsx
@@ -0,0 +1,23 @@
+import Otp from "./otp";
+
+function promoterVerify() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-12 h-screen w-screen">
+        <div className="hidden md:block md:col-span-7">
+          <img
+            src="/bhaktapur.jpg"
+            alt="bhaktapur"
+            className="h-full w-full object-cover rounded-r-3xl"
+          />
+        </div>
+
+        <div className="col-span-1 md:col-span-5 flex flex-col items-center justify-center p-6">
+          <Otp />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default promoterVerify;
