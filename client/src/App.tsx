30f32dc76bc345638b6bc35ca9afa7e625795397
Presab Khadka <presabkhadka30@gmail.com>
2025-02-08 01:58:50 +0545

    installed toast from shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,25 @@ import PromoterLogin from "./pages/promoterLogin";
 import PromoterRegister from "./pages/promoterRegister";
 import PromoterVerify from "./pages/promoterVerify";
 import PromoterDashboard from "./pages/promoterDashboard";
+import { Toaster } from "@/components/ui/toaster";
+import AdminLogin from "./pages/adminLogin";
 
 function App() {
   return (
-    <RouterProvider
-      router={createBrowserRouter([
-        { path: "/explorer/login", element: <Login /> },
-        { path: "/promoter/login", element: <PromoterLogin /> },
-        { path: "/explorer/signup", element: <Register /> },
-        { path: "/promoter/signup", element: <PromoterRegister /> },
-        { path: "/promoter/verify", element: <PromoterVerify /> },
-        { path: "/promoter/dashboard", element: <PromoterDashboard /> },
-      ])}
-    ></RouterProvider>
+    <>
+      <RouterProvider
+        router={createBrowserRouter([
+          { path: "/explorer/login", element: <Login /> },
+          { path: "/promoter/login", element: <PromoterLogin /> },
+          { path: "/admin/login", element: <AdminLogin /> },
+          { path: "/explorer/signup", element: <Register /> },
+          { path: "/promoter/signup", element: <PromoterRegister /> },
+          { path: "/promoter/verify", element: <PromoterVerify /> },
+          { path: "/promoter/dashboard", element: <PromoterDashboard /> },
+        ])}
+      ></RouterProvider>
+      <Toaster />
+    </>
   );
 }
 
