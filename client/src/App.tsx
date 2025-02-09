2932d6ae1055aa064edffaf372b59129f1cbe829
Presab Khadka <presabkhadka30@gmail.com>
2025-02-09 18:26:02 +0545

    installing dark mode theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,25 @@ import PromoterVerify from "./pages/promoterVerify";
 import PromoterDashboard from "./pages/promoterDashboard";
 import { Toaster } from "@/components/ui/toaster";
 import AdminLogin from "./pages/adminLogin";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
     <>
-      <RouterProvider
-        router={createBrowserRouter([
-          { path: "/explorer/login", element: <Login /> },
-          { path: "/promoter/login", element: <PromoterLogin /> },
-          { path: "/admin/login", element: <AdminLogin /> },
-          { path: "/explorer/signup", element: <Register /> },
-          { path: "/promoter/signup", element: <PromoterRegister /> },
-          { path: "/promoter/verify", element: <PromoterVerify /> },
-          { path: "/promoter/dashboard", element: <PromoterDashboard /> },
-        ])}
-      ></RouterProvider>
-      <Toaster />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider
+          router={createBrowserRouter([
+            { path: "/explorer/login", element: <Login /> },
+            { path: "/promoter/login", element: <PromoterLogin /> },
+            { path: "/admin/login", element: <AdminLogin /> },
+            { path: "/explorer/signup", element: <Register /> },
+            { path: "/promoter/signup", element: <PromoterRegister /> },
+            { path: "/promoter/verify", element: <PromoterVerify /> },
+            { path: "/promoter/dashboard", element: <PromoterDashboard /> },
+          ])}
+        ></RouterProvider>
+        <Toaster />
+      </ThemeProvider>
     </>
   );
 }
