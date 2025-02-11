2e59c36d7cd8394ad4f3da6b5c3b9937f9f9f4fb
Presab Khadka <presabkhadka30@gmail.com>
2025-02-12 05:00:04 +0545

    feat promoter dashboard cards

--- client/src/pages/promoterDashboard.tsx
+++ client/src/pages/promoterDashboard.tsx
@@ -1,12 +1,24 @@
 import PromoterNavbar from "@/components/promoterNavbar";
-import { StatsRing } from "@/components/ui/stats-ring";
 
 export default function PromoterDashboard() {
   return (
     <div className="grid grid-cols-1 h-screen md:grid md:grid-cols-12 md: gap-x-4">
       <div className="col-span-full ">
         <PromoterNavbar />
-        <StatsRing />
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+              <h1>total treasure</h1>
+            </div>
+            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+              <h1>positive response</h1>
+            </div>
+            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+              <h1>negative response</h1>
+            </div>
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
       </div>
     </div>
   );
