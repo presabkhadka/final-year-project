01997a3e9d517173a0de196020ef933c5a7933b5
Presab Khadka <presabkhadka30@gmail.com>
2025-02-11 11:49:06 +0545

    updt - promoter dashboard page

--- client/src/pages/promoterDashboard.tsx
+++ client/src/pages/promoterDashboard.tsx
@@ -1,15 +1,12 @@
 import PromoterNavbar from "@/components/promoterNavbar";
+import { StatsRing } from "@/components/ui/stats-ring";
 
 export default function PromoterDashboard() {
   return (
     <div className="grid grid-cols-1 h-screen md:grid md:grid-cols-12 md: gap-x-4">
       <div className="col-span-full ">
         <PromoterNavbar />
-        <div className="p-6 flex gap-4 justify-center items-center">
-          <p>stats a</p>
-          <p>stats b</p>
-          <p>stats c</p>
-        </div>
+        <StatsRing />
       </div>
     </div>
   );
