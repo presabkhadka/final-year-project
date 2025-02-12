b635e460e1713eb5c457286cfc8ab867fd9e31b0
Presab Khadka <presabkhadka30@gmail.com>
2025-02-13 01:33:30 +0545

    updt - promoter dashboard responsive

--- client/src/pages/promoterDashboard.tsx
+++ client/src/pages/promoterDashboard.tsx
@@ -6,18 +6,19 @@ export default function PromoterDashboard() {
       <div className="col-span-full ">
         <PromoterNavbar />
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
-              <h1>total treasure</h1>
+          <div className="grid auto-rows-min gap-4 grid-cols-2 md:grid-cols-3">
+            <div className="grid gap-2 col-span-2 md:col-span-1">
+              <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+                <h1>total treasure</h1>
+              </div>
+              <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+                <h1>total treasure</h1>
+              </div>
             </div>
-            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
+            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center col-span-2">
               <h1>positive response</h1>
             </div>
-            <div className="aspect-video rounded-xl shadow-xl bg-muted/50 flex justify-center items-center">
-              <h1>negative response</h1>
-            </div>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
       </div>
     </div>
