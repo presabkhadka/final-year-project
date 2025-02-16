004cc3035d678c60dec491179d31f8edcfa8b9c0
Presab Khadka <presabkhadka30@gmail.com>
2025-02-16 16:13:15 +0545

    feat - intergrated apis for fetching good rated treasures

--- client/src/pages/promoterDashboard.tsx
+++ client/src/pages/promoterDashboard.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 
 export default function PromoterDashboard() {
   const [totalTreasure, setTotalTreasure] = useState(0);
-  const [ranking, setRanking] = useState(null);
+  const [goodTreasure, setGoodTreasure] = useState(0);
+  const [badTreasure, setBadTreasure] = useState(0);
 
   useEffect(() => {
     const token = localStorage.getItem("Authorization");
@@ -31,36 +32,64 @@ export default function PromoterDashboard() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const token = localStorage.getItem("Authorization")?.split(" ")[1];
+    let fetchGoodTreasure = async () => {
+      try {
+        let response = await axios.get(
+          "http://localhost:1010/promoter/good-treasures",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setGoodTreasure(response.data.goodTreasuresLength);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchGoodTreasure();
+    let interval = setInterval(fetchGoodTreasure, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="grid grid-cols-1 h-screen md:grid md:grid-cols-12 gap-x-4">
       <div className="col-span-full">
         <PromoterNavbar />
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-4">
-          {/* Total Treasures Card */}
-          <div className="rounded-2xl shadow-lg bg-muted/80 flex flex-col items-center hover:shadow-xl overflow-hidden min-h-[10rem]">
-            <img
-              src="../../public/adventure.jpg"
-              alt="adventure"
-              className="w-full max-h-40 md:max-h-48 rounded-t-2xl"
-            />
+          <div className="rounded-xl shadow-lg bg-muted/80 flex justify-center items-center hover:shadow-xl min-h-[10rem]">
             <div className="w-full flex flex-col gap-2 py-2 justify-center items-center">
-              <h1 className="font-bold text-xl sm:text-2xl md:text-3xl text-gray-800 dark:text-gray-200">
+              <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
                 Total Treasures
               </h1>
-              <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-indigo-600 dark:text-indigo-400">
+              <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
                 {totalTreasure}
               </h1>
             </div>
           </div>
 
-          {/* Another Card (e.g., Donations, Statistics, etc.) */}
           <div className="rounded-xl shadow-lg bg-muted/80 flex justify-center items-center hover:shadow-xl min-h-[10rem]">
-            <h1 className="font-bold text-xl sm:text-2xl">Total Treasure</h1>
+          <div className="w-full flex flex-col gap-2 py-2 justify-center items-center">
+              <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
+                Good Treasures
+              </h1>
+              <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
+                {goodTreasure}
+              </h1>
+            </div>
           </div>
 
-          {/* Positive Responses */}
           <div className="rounded-xl shadow-lg bg-muted/80 flex justify-center items-center hover:shadow-xl col-span-1 sm:col-span-2 md:col-span-1 min-h-[10rem]">
-            <h1 className="font-bold text-xl sm:text-2xl">Positive Response</h1>
+          <div className="w-full flex flex-col gap-2 py-2 justify-center items-center">
+              <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
+                Bad Treasures
+              </h1>
+              <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
+                {badTreasure}
+              </h1>
+            </div>
           </div>
         </div>
       </div>
