e80ac8db6c7f699816abc504955fed26292fd587
Presab Khadka <presabkhadka30@gmail.com>
2025-02-16 15:30:40 +0545

    updt - adjusted navbar paddings for small screen

--- client/src/components/promoterNavbar.tsx
+++ client/src/components/promoterNavbar.tsx
@@ -12,7 +12,7 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function PromoterNavbar() {
   return (
-    <div className="py-4 px-6 shadow-md rounded-lg flex justify-between items-center">
+    <div className="py-4 px-4 md:px-6 shadow-md rounded-lg flex justify-between items-center">
       <h1 className="text-2xl font-bold">Urban Discovories</h1>
       <div className="flex gap-4 lg:hidden">
         <Sheet>
