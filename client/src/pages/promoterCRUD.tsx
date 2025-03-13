e3b959c3217d6d3fb96cffca63983a9d31982ea0
Presab Khadka <presabkhadka30@gmail.com>
2025-03-13 20:41:34 +0545

    updt - made forms responsive for all screen size

--- client/src/pages/promoterCRUD.tsx
+++ client/src/pages/promoterCRUD.tsx
@@ -1,43 +1,39 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 interface TreasureFormProps {
-  treasure?: any; // Optional prop for updating an existing treasure
+  treasure?: any; // Optional: Pass an existing treasure for update
+  onSubmit: (formData: FormData) => Promise<void>; // Function to handle submit
 }
 
-const TreasureForm: React.FC<TreasureFormProps> = ({ treasure }) => {
-  const [treasureName, setTreasureName] = useState<string>(
+const TreasureForm: React.FC<TreasureFormProps> = ({ treasure, onSubmit }) => {
+  const [treasureName, setTreasureName] = useState(
     treasure?.treasureName || ""
   );
-  const [treasureLocation, setTreasureLocation] = useState<string>(
+  const [treasureLocation, setTreasureLocation] = useState(
     treasure?.treasureLocation || ""
   );
-  const [treasureDescription, setTreasureDescription] = useState<string>(
+  const [treasureDescription, setTreasureDescription] = useState(
     treasure?.treasureDescription || ""
   );
-
-  const [treasureContact, setTreasureContact] = useState<string>(
+  const [treasureContact, setTreasureContact] = useState(
     treasure?.treasureContact || ""
   );
-
-  const [treasureType, setTreasureType] = useState<string>(
+  const [treasureType, setTreasureType] = useState(
     treasure?.treasureType || ""
   );
-
-  const [openingTime, setOpeningTime] = useState<string>(
+  const [openingTime, setOpeningTime] = useState(
     treasure?.treasureOpeningTime || ""
   );
-
-  const [closingTime, setClosingTime] = useState<string>(
+  const [closingTime, setClosingTime] = useState(
     treasure?.treasureClosingTime || ""
   );
-
   const [treasureImage, setTreasureImage] = useState<File | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -49,7 +45,6 @@ const TreasureForm: React.FC<TreasureFormProps> = ({ treasure }) => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError(null);
 
     const formData = new FormData();
     formData.append("treasureName", treasureName);
@@ -63,164 +58,138 @@ const TreasureForm: React.FC<TreasureFormProps> = ({ treasure }) => {
       formData.append("treasureImage", treasureImage);
     }
 
-    try {
-      const token = localStorage.getItem("Authorization")?.split(" ")[1];
-
-      const response = await fetch(
-        "http://localhost:1010/promoter/add-treasure",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          body: formData,
-        }
-      );
-
-      const data = await response.json();
-
-      if (response.ok) {
-        toast.success(data.msg);
-        navigate("/promoter/dashboard");
-      } else {
-        setError(data.msg || "Something went wrong.");
-      }
-    } catch (error) {
-      setError("Something went wrong while submitting the form.");
-    } finally {
-      setLoading(false);
-    }
+    await onSubmit(formData);
+    setLoading(false);
+    navigate("/promoter/dashboard");
   };
 
   return (
-    <div className="p-6 flex flex-col gap-4">
-      <h1 className="text-2xl font-bold col-span-2">Add New Treasure</h1>
-      {error && <p className="text-red-500">{error}</p>}
-      <form onSubmit={handleSubmit} className="md:grid md:grid-cols-2 md:gap-2">
-        <div className="col-span-1">
-          <label htmlFor="treasureName" className="block">
-            Treasure Name
-          </label>
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg flex flex-col gap-4 sm:grid sm:grid-cols-2"
+    >
+      <h1 className="text-2xl font-bold col-span-full text-center">
+        {treasure ? "Update Treasure" : "Add New Treasure"}
+      </h1>
 
-          <Input
-            type="text"
-            id="treasureName"
-            value={treasureName}
-            onChange={(e) => setTreasureName(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="treasureName" className="font-medium">
+          Treasure Name
+        </label>
+        <Input
+          type="text"
+          value={treasureName}
+          onChange={(e) => setTreasureName(e.target.value)}
+          placeholder="Treasure Name"
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureLocation" className="block">
-            Treasure Location
-          </label>
-          <Input
-            type="text"
-            id="treasureLocation"
-            value={treasureLocation}
-            onChange={(e) => setTreasureLocation(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="treasureLocation" className="font-medium">
+          Treasure Location
+        </label>
+        <Input
+          type="text"
+          value={treasureLocation}
+          onChange={(e) => setTreasureLocation(e.target.value)}
+          placeholder="Location"
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureDescription" className="block">
-            Treasure Description
-          </label>
-          <Textarea
-            id="treasureDescription"
-            value={treasureDescription}
-            onChange={(e) => setTreasureDescription(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="treasureType" className="font-medium">
+          Treasure Type
+        </label>
+        <Input
+          type="text"
+          value={treasureType}
+          onChange={(e) => setTreasureType(e.target.value)}
+          placeholder="Type"
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureDescription" className="block">
-            Treasure Contact
-          </label>
-          <Textarea
-            id="treasureContact"
-            value={treasureContact}
-            onChange={(e) => setTreasureContact(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="treasureContact" className="font-medium">
+          Treasure Contact
+        </label>
+        <Input
+          type="text"
+          value={treasureContact}
+          onChange={(e) => setTreasureContact(e.target.value)}
+          placeholder="Contact"
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureOpeningTime" className="block">
-            Opening Time
-          </label>
-          <Input
-            type="text"
-            id="treasureOpeningTime"
-            value={openingTime}
-            onChange={(e) => setOpeningTime(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
-        <div className="col-span-1">
-          <label htmlFor="treasureClosingTime" className="block">
-            Closing Time
-          </label>
-          <Input
-            type="text"
-            id="treasureClosingTime"
-            value={closingTime}
-            onChange={(e) => setClosingTime(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="openingTime" className="font-medium">
+          Opening Time
+        </label>
+        <Input
+          type="time"
+          value={openingTime}
+          onChange={(e) => setOpeningTime(e.target.value)}
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureType" className="block">
-            Treasure Type
-          </label>
-          <Input
-            type="text"
-            id="treasureType"
-            value={treasureType}
-            onChange={(e) => setTreasureType(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="closingTime" className="font-medium">
+          Closing Time
+        </label>
+        <Input
+          type="time"
+          value={closingTime}
+          onChange={(e) => setClosingTime(e.target.value)}
+          className="w-full p-2 border rounded-md"
+          required
+        />
+      </div>
 
-        <div className="col-span-1">
-          <label htmlFor="treasureImage" className="block">
-            Treasure Image
-          </label>
-          <Input
-            type="file"
-            id="treasureImage"
-            name="treasureImage"
-            onChange={handleImageChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
+      <div className="flex flex-col gap-1 col-span-full">
+        <label htmlFor="treasureDescription" className="font-medium">
+          Treasure Description
+        </label>
+        <Textarea
+          value={treasureDescription}
+          onChange={(e) => setTreasureDescription(e.target.value)}
+          placeholder="Description"
+          className="w-full p-2 border rounded-md resize-none"
+          required
+        />
+      </div>
 
-        <div className="mt-4 col-span-full flex justify-center">
-          <button
-            type="submit"
-            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-400"
-            disabled={loading}
-          >
-            {loading
-              ? "Submitting..."
-              : treasure
-              ? "Update Treasure"
-              : "Add Treasure"}
-          </button>
-        </div>
-      </form>
-    </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="treasureImage" className="font-medium">
+          Treasure Image
+        </label>
+        <Input
+          type="file"
+          onChange={handleImageChange}
+          className="w-full p-2 border rounded-md"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="col-span-full w-full px-4 py-2 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 transition disabled:bg-gray-400"
+        disabled={loading}
+      >
+        {loading
+          ? "Submitting..."
+          : treasure
+          ? "Update Treasure"
+          : "Add Treasure"}
+      </button>
+    </form>
   );
 };
 
