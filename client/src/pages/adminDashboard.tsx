cb72642e67a1510820ff120cb85f6c1e60f3b165
Presab Khadka <presabkhadka30@gmail.com>
2025-03-16 00:12:57 +0545

    feat - admin dashboard started

--- client/src/pages/adminDashboard.tsx
+++ client/src/pages/adminDashboard.tsx
@@ -1,3 +1,154 @@
+import AdminNavbar from "@/components/AdminNavbar";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
 export default function AdminDashboard() {
-  return <h1>hi there this is admin dashboard</h1>;
+  const [explorer, setExplorer] = useState(null);
+  const [promoter, setPromoter] = useState(null);
+  const [treasure, setTreasure] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("Authorization")?.split(" ")[1];
+    if (!token) {
+      throw new Error("No authentication token found");
+    }
+    let fetchTotalExplorer = async () => {
+      try {
+        let response = await axios.get(
+          "http://localhost:1010/admin/total-explorer",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setExplorer(response.data.totalExplorers);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTotalExplorer();
+    let interval = setInterval(fetchTotalExplorer, 2000);
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("Authorization")?.split(" ")[1];
+    if (!token) {
+      throw new Error("No authentication token found");
+    }
+    let fetchTotalExplorer = async () => {
+      try {
+        let response = await axios.get(
+          "http://localhost:1010/admin/total-promoter",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setPromoter(response.data.totalPromoter);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTotalExplorer();
+    let interval = setInterval(fetchTotalExplorer, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("Authorization")?.split(" ")[1];
+    if (!token) {
+      throw new Error("No authentication token found");
+    }
+    let fetchTotalExplorer = async () => {
+      try {
+        let response = await axios.get(
+          "http://localhost:1010/admin/total-treasure",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setTreasure(response.data.totalTreasure);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTotalExplorer();
+    let interval = setInterval(fetchTotalExplorer, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "hsl(var(--chart-1))",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "hsl(var(--chart-2))",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <div className="grid grid-cols-1 h-screen md:grid md:grid-cols-12 gap-x-4">
+      <div className="col-span-full">
+        <div className="bg-white shadow-md dark:bg-black sticky top-0 z-50">
+          <AdminNavbar />
+        </div>
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-4">
+          <CardWrapper>
+            <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
+              Total Explorer
+            </h1>
+            <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
+              {explorer}
+            </h1>
+          </CardWrapper>
+          <CardWrapper>
+            <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
+              Total Promoter
+            </h1>
+            <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
+              {promoter}
+            </h1>
+          </CardWrapper>
+          <CardWrapper>
+            <h1 className="font-bold text-xl sm:text-2xl md:text-2xl text-gray-800 dark:text-gray-200">
+              Total Treasure
+            </h1>
+            <h1 className="font-semibold text-2xl sm:text-2xl md:text-3xl text-indigo-600 dark:text-indigo-400">
+              {treasure}
+            </h1>
+          </CardWrapper>
+         
+        </div>
+      </div>
+    </div>
+  );
+  function CardWrapper({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="rounded-xl shadow-lg bg-muted/80 flex flex-col justify-center items-center hover:shadow-xl min-h-[10rem] gap-2">
+        {children}
+      </div>
+    );
+  }
 }
