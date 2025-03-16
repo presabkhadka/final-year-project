b6a097200e71a5a34ae26b91ab19c77f098415fb
Presab Khadka <presabkhadka30@gmail.com>
2025-03-16 22:03:36 +0545

    feat - functioning logout button

--- client/src/components/AdminNavbar.tsx
+++ client/src/components/AdminNavbar.tsx
@@ -6,11 +6,57 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { NavLink } from "react-router-dom";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { NavLink, useNavigate } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function AdminNavbar() {
+  interface AdminDetails {
+    adminName?: String;
+    adminEmail?: String;
+  }
+
+  const [adminDetails, setAdminDetails] = useState<AdminDetails>({});
+  const navigate = useNavigate();
+
+  // ue for fetching the admin details
+  useEffect(() => {
+    let fetchAdminDetails = async () => {
+      try {
+        let token = localStorage.getItem("Authorization")?.split(" ")[1];
+        const response = await axios.get(
+          "http://localhost:1010/admin/admin-details",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setAdminDetails(response.data.admin);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAdminDetails();
+    let interval = setInterval(fetchAdminDetails, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("Authorization");
+    localStorage.removeItem("UserRole");
+    navigate("/admin/login");
+    toast.success("Logged out successfully");
+  };
+
   return (
     <div className="py-4 px-4 md:px-6 shadow-md rounded-lg flex justify-between items-center dark:border">
       <h1 className="text-2xl font-bold">Urban Discovories</h1>
@@ -103,25 +149,45 @@ export default function AdminNavbar() {
             </SheetHeader>
           </SheetContent>
         </Sheet>
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
-          </AvatarFallback>
-        </Avatar>
+        <Popover>
+          <PopoverTrigger>
+            <Avatar>
+              <AvatarFallback>
+                {adminDetails.adminName?.charAt(0) || (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                )}
+              </AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="w-56 p-4  shadow-lg rounded-xl border ">
+            <div className="text-sm ">
+              <p className="font-semibold">
+                {adminDetails?.adminName || "Admin"}
+              </p>
+              <p className="text-xs ">{adminDetails?.adminEmail}</p>
+
+              <button
+                onClick={handleLogout}
+                className="mt-4 w-full px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200"
+              >
+                Log Out
+              </button>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
       <div className="hidden lg:flex lg:gap-2">
         <NavLink
@@ -189,40 +255,45 @@ export default function AdminNavbar() {
           </SheetContent>
         </Sheet>
         <ModeToggle />
-        <Avatar>
-          <AvatarImage>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
-          </AvatarImage>
-          <AvatarFallback>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
-          </AvatarFallback>
-        </Avatar>
+        <Popover>
+          <PopoverTrigger>
+            <Avatar>
+              <AvatarFallback>
+                {adminDetails.adminName?.charAt(0) || (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                )}
+              </AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="w-56 p-4  shadow-lg rounded-xl border ">
+            <div className="text-sm ">
+              <p className="font-semibold">
+                {adminDetails?.adminName || "Admin"}
+              </p>
+              <p className="text-xs ">{adminDetails?.adminEmail}</p>
+
+              <button
+                onClick={handleLogout}
+                className="mt-4 w-full px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200"
+              >
+                Log Out
+              </button>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
