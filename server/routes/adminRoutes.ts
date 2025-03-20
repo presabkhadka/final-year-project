eabbf54eed10a719339e891882b7ff57a6eb5097
Presab Khadka <presabkhadka30@gmail.com>
2025-03-21 00:28:10 +0545

    feat - integrating multer in admin routes

--- server/routes/adminRoutes.ts
+++ server/routes/adminRoutes.ts
@@ -3,7 +3,6 @@ import adminMiddleware from "../middleware/adminMiddleware";
 import {
   adminSignup,
   adminLogin,
-  addDonation,
   reviewReviews,
   deleteTreasures,
   totalExplorer,
@@ -11,12 +10,41 @@ import {
   totalTreasure,
   adminDetails,
   treasureReviews,
+  addDonation,
+  fetchDonation,
+  updateCampaign,
 } from "../controller/adminController";
 import multer from "multer";
 import path from "path";
 const router = Router();
 
-const upload = multer({ dest: path.join(__dirname, "../uploads/") });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join("D:", "final-year-project", "server", "uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+// Create the multer instance
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files are allowed!"));
+    }
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 router.post("/signup", adminSignup);
 router.post("/login", adminLogin);
@@ -33,5 +61,12 @@ router.get("/total-promoter", adminMiddleware, totalPromoter);
 router.get("/total-treasure", adminMiddleware, totalTreasure);
 router.get("/admin-details", adminMiddleware, adminDetails);
 router.get("/treasure-details", adminMiddleware, treasureReviews);
+router.get("/active-donation-campaigns", adminMiddleware, fetchDonation);
+router.patch(
+  "/update-donation-campaign/:donationId",
+  upload.single("donationQR"),
+  adminMiddleware,
+  updateCampaign
+);
 
 export { router };
