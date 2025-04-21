89fc89046513f7374e9e09cd5b58b499295a1166
Presab Khadka <presabkhadka30@gmail.com>
2025-04-21 12:00:00 +0545

    feat - notification integration

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -54,6 +54,8 @@ export default function Navbar() {
     return () => clearInterval(interval);
   }, []);
 
+  
+
   const handleLogout = () => {
     localStorage.removeItem("Authorization");
     localStorage.removeItem("UserRole");
