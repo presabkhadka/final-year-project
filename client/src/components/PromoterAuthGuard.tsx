0575e28b3548c853d91179944ee1f503cff8283b
presabkhadka <presabkhadka30@gmail.com>
2025-05-02 12:00:00 +0545

    updt - adding comments

--- client/src/components/PromoterAuthGuard.tsx
+++ client/src/components/PromoterAuthGuard.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
+// check the token from the headers
 const isAuthentitcated = () => {
   return !!localStorage.getItem("Authorization");
 };
 
+// get the user role from the headers
 const getUserRole = () => {
   return localStorage.getItem("UserRole");
 };
